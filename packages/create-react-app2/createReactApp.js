c621401b167feb8ce9bf8e425d07a00357c8b5b8
chenliupeng <26234020@qq.com>
2021-10-28 03:06:31 +0800

    feat: projectName 参数处理

---
+++ packages/create-react-app2/createReactApp.js
@@ -0,0 +1,41 @@
+const {Command } = require('commander');
+const chalk = require('chalk');
+const packageJSON = require('./package.json');
+const  fs  = require('fs-extra'); // 加强版 增删改查
+const path = require('path');
+async function init() {
+    let projectName;
+    new Command(packageJSON.name) // 项目明
+    .version(packageJSON.version) // 版本号
+    .arguments('<project-directory>') // 项目目录名
+    .usage(`${chalk.green('<project-directory>')}`)
+    .action((name) => {
+        projectName = name;
+    }).parse(process.argv) // node 完整路径，当前node脚本的路径，...其他参数
+    console.log(projectName)
+    await createApp(projectName)
+}
+
+module.exports = {
+    init
+}
+
+async function createApp(appName) { // projectName
+    let root = path.resolve(appName); // 得到将生成项目的绝对目录
+    fs.ensureDirSync(appName); // 保证此目录是存在，如果不存在，则创建
+    console.log(`Creating a new React app in ${chalk.green(root)}`);
+    const packageJSON = {
+        name:appName,
+        version:'0.1.0',
+        private:true
+    }
+    fs.writeFileSync(
+        path.join(root, 'package.json'),
+        JSON.stringify(packageJSON,null,2)
+    );
+    const originalDirectory = process.cwd(); // 原始的命令工作目录
+    process.chdir(root); 
+    console.log('appName',appName);
+    console.log('root',root);
+    console.log('originalDirectory',originalDirectory);
+}
